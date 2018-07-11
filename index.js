7a4a1c2b18d870821c50e5410e48515d2288e30e
Shane Thacker <Shane@shanes-mbp.attlocal.net>
2018-07-11 15:15:49 -0500

    WIP

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const toURL = (url, params) => url.includes('?') ? url : url + params;
 
 module.exports = (table = {}) => {
-  return {
-    reset(newTable = {}) {
-      table = newTable;
-    },
-
-    handle(req, res, next) {
+  return (req, res, next) => {
       const url = req.url;
       const host = req.host;
       const hostname = req.hostname;
@@ -25,6 +20,32 @@ module.exports = (table = {}) => {
       } else {
         next();
       }
-    }
   }
+
+  // return {
+  //   reset(newTable = {}) {
+  //     table = newTable;
+  //   },
+
+  //   handle(req, res, next) {
+  //     const url = req.url;
+  //     const host = req.host;
+  //     const hostname = req.hostname;
+  //     const params = req.search || '';
+
+  //     if (table[url]) {
+  //       res.status(301).redirect(toURL(table[url], params));
+  //     } else if (table[hostname + url]) {
+  //       res.status(301).redirect(toURL(table[hostname + url], params));
+  //     } else if (table[host + url]) {
+  //       res.status(301).redirect(toURL(table[host + url], params));
+  //     } else if (table[hostname]) {
+  //       res.status(301).redirect(toURL(table[hostname] + url, params));
+  //     } else if (table[host]) {
+  //       res.status(301).redirect(toURL(table[host] + url, params));
+  //     } else {
+  //       next();
+  //     }
+  //   }
+  // }
 }
